e043d506e44963f6f554c21da5670d0ddbfe2038
tsimur <2494035@gmail.com>
2020-05-15 17:31:24 +0300

    reset

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -33,9 +33,7 @@
 		<header id="site-header" class="header-footer-group" role="banner">
 
 			<div class="header-inner section-inner">
-				<?php
-				echo "Dev code new"
-				?>
+
 
 				<div class="header-titles-wrapper">
 
